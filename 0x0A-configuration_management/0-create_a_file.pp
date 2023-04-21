41acb1d5a7e3ca01efa1f1ddecd4e0c2e02f0f05
Dut <dutandrew78@gmail.com>
2023-04-21 12:43:27 +0300

    creating a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # creating a file
 
 file { 'school':
-     ensure => 'present',
-     mode => '0744',
-     owner => 'www-data',
-     group => 'www-data',
-     content => "I love Puppet"
+  ensure  => 'present',  
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
