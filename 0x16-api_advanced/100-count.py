e263c55c55d280964302bd23e0377714fbfecf1e
Dut Kulang <69721309+dutkulang@users.noreply.github.com>
2023-08-10 22:17:13 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,66 +1,72 @@
 #!/usr/bin/python3
-'''
-Recurse function find the words inside the title
-'''
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import re
 import requests
+import sys
 
 
-def print_result(obj):
-    if not obj:
-        return obj
-    for elem in sorted(obj.items()):
-        print('{}: {}'.format(elem[0], elem[1]))
-    return obj
+def add_title(dictionary, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+
+    title = hot_posts[0]['data']['title'].split()
+    for word in title:
+        for key in dictionary.keys():
+            c = re.compile("^{}$".format(key), re.I)
+            if c.findall(word):
+                dictionary[key] += 1
+    hot_posts.pop(0)
+    add_title(dictionary, hot_posts)
 
 
-def count_words(subreddit, word_list, after=None, obj={}):
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3)\
-                  AppleWebKit/537.36 (KHTML, like Gecko)\
-                  Chrome/35.0.1916.47 Safari/537.36'
-    headers = {'User-Agent': user_agent}
-    #print(after)
-    #print(obj)
-    if after is None:
-        request = requests.get('https://www.reddit.com/r/{}/hot.json?limit=100'
-                               .format(subreddit), headers=headers)
-        if request.status_code != 200:
-            return None
-        data = request.json().get('data')
-        childrens = data.get('children')
-        if not childrens:
-            return None
-        if not word_list:
-            return None
+def recurse(subreddit, dictionary, after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
 
-        for c in childrens:
-            for word in word_list:
-                count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word),
-                            c.get('data').get('title'), re.IGNORECASE))
-                if count > 0:
-                    obj[word] = count
-        if data.get('after') is None:
-            print_result(obj)
+    params = {
+        'after': after
+    }
 
-        count_words(subreddit, word_list, data.get('after'), obj)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return None
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(dictionary, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return
+    recurse(subreddit, dictionary, after=after)
+
+
+def count_words(subreddit, word_list):
+    """ Init function """
+    dictionary = {}
+
+    for word in word_list:
+        dictionary[word] = 0
+
+    recurse(subreddit, dictionary)
+
+    l = sorted(dictionary.items(), key=lambda kv: kv[1])
+    l.reverse()
+
+    if len(l) != 0:
+        for item in l:
+            if item[1] is not 0:
+                print("{}: {}".format(item[0], item[1]))
     else:
-        url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'
-        request = requests.get(url.format(subreddit, after), headers=headers)
-        data = request.json().get('data')
-        childrens = data.get('children')
-
-        for c in childrens:
-            for word in word_list:
-                count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word),
-                            c.get('data').get('title'), re.IGNORECASE))
-                if count > 0:
-                    if word in obj:
-                        obj[word] += count
-                    else:
-                        obj[word] = count
-
-        if data.get('after') is None:
-            print_result(obj)
-        else:
-            count_words(subreddit, word_list, data.get('after'), obj)
-    #return obj
+        print("")
