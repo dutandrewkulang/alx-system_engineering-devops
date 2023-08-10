87eb2591aa62df59ab4bbfa074c0590318a024c1
Dut <dutandrew78@gmail.com>
2023-08-10 22:20:45 +0300

    editted

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -66,7 +66,7 @@ def count_words(subreddit, word_list):
 
     if len(l) != 0:
         for item in l:
-            if item[1] is not 0:
+            if item[1] != 0:
                 print("{}: {}".format(item[0], item[1]))
     else:
         print("")
